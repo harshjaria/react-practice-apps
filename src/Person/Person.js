eae4763d294edb178c09e14ef4d6bdd13ba28c03
unknown <jaria910@gmail.com>
2018-06-14 09:34:33 +0530

    jhgshfg

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Person = (props)=> {
-    return <p>Hi My self {props.name} and my age is  {props.age} years old !</p>
-} ;
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
 
-export default Person ;
+export default person;
